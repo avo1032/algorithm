f1083c4ba98e58ffd13ea3b7ae3fb0949d244b20
유승완 <swan1032@naver.com>
2022-12-04 19:45:21 +0900

    baekjoon10807

---
+++ baekjoon/baekjoon10807.js
@@ -0,0 +1,43 @@
+/* 백준 2869번
+총 N개의 정수가 주어졌을 때, 정수 v가 몇 개인지 구하는 프로그램을 작성하시오.
+
+입력 : 첫째 줄에 정수의 개수 N(1 ≤ N ≤ 100)이 주어진다.
+      둘째 줄에는 정수가 공백으로 구분되어져있다. 
+      셋째 줄에는 찾으려고 하는 정수 v가 주어진다. 입력으로 주어지는 정수와 v는 -100보다 크거나 같으며, 100보다 작거나 같다.
+
+출력 : 첫째 줄에 입력으로 주어진 N개의 정수 중에 v가 몇 개인지 출력한다.
+ */
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+let result;
+
+rl.on("line", (line) => {
+  input.push(line);
+
+  const N = input[0];
+  const num = input[1];
+  const v = input[2];
+  let numArr = [];
+  if (num) {
+    numArr = num.split(" ")
+  }
+  if (v) {
+    result = numArr.filter((value) => (value == v));
+    rl.on("close", () => {
+      console.log(result.length)
+    })
+  }
+});
+
+// rl.on("close", () => {
+//   console.log(input);
+//   console.log(result.length);
+//   process.exit();
+// });
