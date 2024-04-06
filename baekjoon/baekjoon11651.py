768f22050fa5fcee200838cc16c03b1d71268ead
avo1032 <swan1032@naver.com>
2024-04-06 23:37:09 +0900

    baekjoon11651

---
+++ baekjoon/baekjoon11651.py
@@ -0,0 +1,14 @@
+import sys
+
+
+n = int(input())
+
+data_list = []
+for i in range(n):
+    data = list(map(int, sys.stdin.readline().split()))
+    data_list.append(data)
+
+sorted_data = sorted(data_list, key=lambda x: (x[1], x[0]))
+for j in sorted_data:
+    print(j[0], end=" ")
+    print(j[1])
