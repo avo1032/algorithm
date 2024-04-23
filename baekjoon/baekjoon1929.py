a91a1a1cfff9bc0c8688da2157ebef78ee90cd05
avo1032 <swan1032@naver.com>
2024-04-23 23:31:07 +0900

    baekjoon1929

---
+++ baekjoon/baekjoon1929.py
@@ -0,0 +1,18 @@
+import sys
+import math
+
+
+def is_prime(x):
+    if x == 1:
+        return False
+    for i in range(2, int(math.sqrt(x) + 1)):
+        if x % i == 0:
+            return False
+    return True
+
+
+m, n = map(int, sys.stdin.readline().split())
+
+for i in range(m, n + 1):
+    if is_prime(i):
+        print(i)
